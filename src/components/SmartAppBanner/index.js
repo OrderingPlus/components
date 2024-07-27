797e08f60418e77c63cdf6cf3477084dd48d3223
Jesus David <jesusdpch00@gmail.com>
2024-07-27 17:37:57 -0400

    feat: add smart app banner with dynamic meta tags
    The code changes in this commit add a smart app banner component to the project. The component dynamically generates meta tags based on the configuration and theme settings. This allows for a customizable and interactive app banner experience for users.

--- src/components/SmartAppBanner/index.js
+++ src/components/SmartAppBanner/index.js
@@ -1,41 +1,47 @@
 import React, { useEffect } from 'react'
 import { useConfig } from '../../contexts/ConfigContext'
 import PropTypes from 'prop-types'
+import { useLanguage } from '../../contexts/LanguageContext'
+import { useTheme } from 'styled-components'
 
 export const SmartAppBanner = (props) => {
   props = { ...defaultProps, ...props }
   const {
     UIComponent,
     storeAndroidId,
-    storeAppleId,
-    storeKindleId
+    storeAppleId
   } = props
 
   const [{ configs }] = useConfig()
+  const theme = useTheme()
+  const [, t] = useLanguage()
 
   useEffect(() => {
     if (!configs) return
+    const logo = theme?.images?.logos?.isotype
     const metas = [
-      {
-        name: 'google-play-app',
-        id: storeAndroidId || configs?.android_app_id?.value
-      },
-      {
-        name: 'apple-itunes-app',
-        id: storeAppleId || configs?.ios_app_id?.value
-      },
-      {
-        name: 'kindle-fire-app',
-        id: storeKindleId || configs?.kindle_app_id?.value
-      }
+      { name: 'smartbanner:title', content: t('MOBILE_APPNAME', 'Mobila appname') },
+      { name: 'smartbanner:author', content: t('MOBILE_APPNAME_AUTHOR', 'App name author') },
+      { name: 'smartbanner:price', content: t('FREE', 'Free') },
+      { name: 'smartbanner:price-suffix-apple', content: ` - ${t('ON_THE_APP_STORE', 'On the app store')}` },
+      { name: 'smartbanner:price-suffix-google', content: ` - ${t('IN_GOOGLE_PLAY', 'In google play')}` },
+      { name: 'smartbanner:icon-apple', content: logo },
+      { name: 'smartbanner:icon-google', content: logo },
+      { name: 'smartbanner:button', content: t('VIEW', 'View') },
+      { name: 'smartbanner:button-url-apple', content: storeAppleId || configs?.android_app_id?.value },
+      { name: 'smartbanner:button-url-google', content: storeAndroidId || configs?.ios_app_id?.value },
+      { name: 'smartbanner:enabled-platforms', content: 'android,ios' },
+      { name: 'smartbanner:close-label', content: t('CLOSE', 'Close') },
+      { name: 'smartbanner:api', content: 'no' }
     ]
-    metas.filter(meta => meta?.id).map(meta => {
-      const metaElement = document.createElement('meta')
-      metaElement.name = meta.name
-      metaElement.content = `app-id=${meta.id}`
-      document.getElementsByTagName('head')[0].appendChild(metaElement)
+    // add metas to head
+    metas.forEach(meta => {
+      const metaTag = document.createElement('meta')
+      metaTag.name = meta.name
+      metaTag.content = meta.content
+      document.head.appendChild(metaTag)
     })
-  }, [configs])
+  }, [configs, theme])
 
   return (
     <>

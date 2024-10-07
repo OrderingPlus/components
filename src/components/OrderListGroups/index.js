995ec9c3744a9c78485753d12e365d8f9194a761
Carlos Gomez <carlos.g@ordering.co>
2024-10-07 15:13:15 -0600

    Refactor order list groups component to improve code readability and maintainability

--- src/components/OrderListGroups/index.js
+++ src/components/OrderListGroups/index.js
@@ -416,11 +416,11 @@ export const OrderListGroups = (props) => {
 
       const _ordersCleaned = error
         ? (newFetch || newFetchCurrent)
-          ? []
-          : sortOrders(ordersGroup[currentTabSelected]?.orders)
+            ? []
+            : sortOrders(ordersGroup[currentTabSelected]?.orders)
         : (newFetch || newFetchCurrent)
-          ? sortOrders(result)
-          : sortOrders(ordersGroup[currentTabSelected]?.orders.concat(result))
+            ? sortOrders(result)
+            : sortOrders(ordersGroup[currentTabSelected]?.orders.concat(result))
       if (options?.allStatusses) {
         setOrdersFiltered({
           error,
@@ -535,14 +535,14 @@ export const OrderListGroups = (props) => {
           error: error ? result : null,
           pagination: !error
             ? {
-              ...ordersGroup[currentTabSelected].pagination,
-              currentPage: pagination.current_page,
-              pageSize: pagination.page_size,
-              totalPages: pagination.total_pages,
-              total: pagination.total,
-              from: pagination.from,
-              to: pagination.to
-            }
+                ...ordersGroup[currentTabSelected].pagination,
+                currentPage: pagination.current_page,
+                pageSize: pagination.page_size,
+                totalPages: pagination.total_pages,
+                total: pagination.total,
+                from: pagination.from,
+                to: pagination.to
+              }
             : ordersGroup[currentTabSelected].pagination
         }
       })

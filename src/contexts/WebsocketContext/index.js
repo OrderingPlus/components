16b7f9020f2a6ad6342d04bd530ee5e0dc37030b
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2024-11-13 12:23:51 -0400

    Refactor ProductForm component to handle null checks and optional chaining

--- src/contexts/WebsocketContext/index.js
+++ src/contexts/WebsocketContext/index.js
@@ -52,8 +52,12 @@ export const WebsocketProvider = ({ settings, children, strategy }) => {
         clearInterval(projectInputInterval)
       }
       if (project) {
-        setConfigs({ ...configs, project })
-        configs.project = project
+        try {
+          const parsedProject = JSON.parse(project)
+          setConfigs((prevConfigs) => ({ ...prevConfigs, project: parsedProject }))
+        } catch (error) {
+          setConfigs((prevConfigs) => ({ ...prevConfigs, project }))
+        }
         clearInterval(projectInputInterval)
       }
     }, 1000)

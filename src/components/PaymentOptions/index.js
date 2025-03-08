b17b3bff6f7ff0619a4092764db453457a0fddd9
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2025-03-08 17:05:45 -0400

    Merge pull request #159 from OrderingPlus/izipay
    feat: Add Izipay to redirect payment methods

--- src/components/PaymentOptions/index.js
+++ src/components/PaymentOptions/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import PropTypes from 'prop-types'
 import { useOrder } from '../../contexts/OrderContext'
 import { useApi } from '../../contexts/ApiContext'
@@ -11,7 +11,7 @@ const stripeLink = 'stripe_link'
 const paymethodsExisting = ['stripe', 'stripe_direct', 'stripe_connect', 'paypal', 'square']
 const paymethodsNotAllowed = ['paypal_express', 'authorize']
 const paymethodsCallcenterMode = ['cash', 'card_delivery', 'ivrpay', '100_coupon', stripeLink]
-const redirectMethods = ['stripe_checkout', 'checkout_pro_mercadopago', 'epayco', 'interswitch']
+const redirectMethods = ['stripe_checkout', 'checkout_pro_mercadopago', 'epayco', 'interswitch', 'izipay']
 
 /**
  * Component to manage payment options behavior without UI component
@@ -44,6 +44,12 @@ export const PaymentOptions = (props) => {
   const [isOpenMethod, setIsOpenMethod] = useState({ paymethod: null })
 
   const requestsState = {}
+
+  const paymethodV2Featured = useMemo(() => {
+    const paymethod = paymethodsList?.paymethods?.find(p => p.gateway === props.paySelected?.gateway)
+    return (paymethod?.version === 'v2' &&
+      paymethod.featured) || ''
+  }, [paymethodsList, props.paySelected])
 
   const filterPaymentMethods = (paymentMethods) => {
     const validations = {
@@ -134,12 +140,13 @@ export const PaymentOptions = (props) => {
     setPaymethodData(data)
     if (Object.keys(data).length) {
       const paymethod = props.paySelected || isOpenMethod.paymethod
+
       setPaymethodsSelected(paymethod)
       onPaymentChange && onPaymentChange({
         paymethodId: paymethod?.id,
         id: paymethod?.id,
         gateway: paymethod?.gateway,
-        paymethod,
+        paymethod: paymethod?.paymethod || paymethod,
         credentials: paymethod?.credentials ?? null,
         data
       })
@@ -151,7 +158,7 @@ export const PaymentOptions = (props) => {
         id: paymethodSelected.id,
         name: paymethodSelected.name,
         gateway: paymethodSelected.gateway,
-        paymethod: paymethodSelected,
+        paymethod: paymethodSelected?.paymethod || paymethodSelected,
         data
       })
     } else {
@@ -184,7 +191,7 @@ export const PaymentOptions = (props) => {
         id: paymethodSelected.id,
         name: paymethodSelected.name,
         gateway: paymethodSelected.gateway,
-        paymethod: paymethodSelected,
+        paymethod: paymethodSelected?.paymethod || paymethodSelected,
         data: paymethodData
       })
     } else if (paymethodSelected === null && onPaymentChange) {
@@ -239,6 +246,7 @@ export const PaymentOptions = (props) => {
           paymethodsList={paymethodsList}
           paymethodSelected={paymethodSelected}
           paymethodData={paymethodData}
+          paymethodV2Featured={paymethodV2Featured}
           setPaymethodData={setPaymethodData}
           handlePaymethodClick={handlePaymethodClick}
           handlePaymethodDataChange={handlePaymethodDataChange}

418b4001b4879571a8fad22d726a33c14db2cd9b
Carlos Gomez <68757729+carlosg-ordering@users.noreply.github.com>
2026-04-13 14:37:05 -0600

    reviert #281 (#287)

--- src/contexts/WebsocketContext/index.js
+++ src/contexts/WebsocketContext/index.js
@@ -28,13 +28,18 @@ export const WebsocketProvider = ({ settings, children, strategy }) => {
         accessToken: session.token,
         url: 'https://sockets.orderingplus.com'
       })
-      _socket.connect()
       setSocket(_socket)
-      return () => {
-        _socket.close()
-      }
     }
-  }, [session.loading, session.token, JSON.stringify(configs), session?.user?.id])
+  }, [session.loading, session.token, JSON.stringify(configs)])
+
+  useEffect(() => {
+    if (socket) {
+      socket.connect()
+    }
+    return () => {
+      socket && socket.close()
+    }
+  }, [socket, session?.user?.id])
 
   useEffect(() => {
     if (session.auth) return
